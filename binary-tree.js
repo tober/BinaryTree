1a8fa5a95707dd3f0c6a8dc66d3796046e67294d
tober <tober@mail.ru>
2016-02-02 13:37:35 +0300

    1

--- binary-tree.js
+++ binary-tree.js
@@ -1,13 +1,68 @@
 'use strict';
 
 class BinaryTree {
+
+
 	constructor() {
 
-	}
 
-	insert(data) {
+	  this.root = null;
 
-	}
+	
+	
+ }
+	insert(data) 
+	{
+	 
+			var node = { 
+                data: data, 
+                left: null,
+                right: null 
+            }, 
+			current;
+			if (this.root === null)
+			{
+            this.root = node;
+			}
+			else {
+				current = this.root;
+					while(true)
+					{
+						if (data < current.data)
+						{
+							if (current.left === null)
+							{
+								current.left = node
+								break;
+							}
+							else
+							{
+							current = current.left
+							}
+						}
+						else
+						{
+                
+							if (current.right === null)
+							{
+								current.right = node;
+							break;
+							}
+							else 
+							{                    
+								current = current.right;
+							}						
+						}
+						       
+ 
+               
+					} 	
+					
+				}
+				
+				
+				}
+
 
 	contains(data) {
 
